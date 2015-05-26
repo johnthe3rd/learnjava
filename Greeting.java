15530b454454a8f97c0d17c317c3004fea7041f7
John <john@johnthethird.net>
2015-05-25 22:06:52 -0500

    Comments added; test for a command line argument; outputs based on test.

--- Greeting.java
+++ Greeting.java
@@ -1,5 +1,13 @@
+//creation of public class for Greeting
 public class Greeting {
+    //main to print to console
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+	if (args.length > 0) {
+            //print out the user's name
+            System.out.println("Hello, " + args[0] + "!");
+	} else {
+            //prints out the default statement.
+            System.out.println("Hello, World!");
+        }
     }
 }
