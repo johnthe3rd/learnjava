512f039b8ed6568f93f36b72990c5d51b48a96a0
John <john.n.brown.iii@gmail.com>
2015-06-04 19:19:59 -0500

    Test prep for error messages.

--- Greeting.java
+++ Greeting.java
@@ -1,33 +1,40 @@
 import java.util.ArrayList;
 
-//creation of public class for Greeting
-public class Greeting{
-    public static void main(String[] args) {
-        //new array of default value and possible arg value
-        //preferred solution. Left commented for length at present. Future features will likely use this implementation.
-        //ArrayList<String> greetingTargets = new ArrayList<String>();
+//public class Greeting{
+//    public static void main(String[] args) {
+//	String Target = (args.length > 0) ? args[0] : "World";
+//	for (int i = 0; i<Target.length();){
+//	    char ch = Target.charAt(i);
+//            if (Character.isLetter(ch)){
+//                i++;
+//            }else{
+//                Target = "World";
+//                } 
+//	    }
+//        System.out.println("Hello, " + Target + "!");
+//    }
+//}
 
-        //greetingTargets.add("World"); // [0]
-        //if (args.length > 0){
-        //greetingTargets.add(args[0]);
-        //}
-        
-        //System.out.println("Hello, " + greetingTargets.get(greetingTargets.size()-1) + "!");
-        
-        //not a scaleable solution. will become hard to read with multiple arguments and difficult to test. 
-	String Target = (args.length > 0) ? args[0] : "World";
-	for (int i = 0; i<Target.length();){
-	    char ch = Target.charAt(i);
-            if (Character.isLetter(ch)){
-                i++;
-            }else{
-                Target = "World";
-                } 
-	    }
+
+
+public class Greeting{
+    public static void main (String[] args) {
+        String Target;
+        if (args.length > 0) {
+            Target = args[0];
+            for (int i = 0; i < Target.length();){
+                char ch = Target.charAt(i);
+                if (Character.isLetter(ch)){
+                    i++;
+                }else{
+                    Target = "World";
+		    break;
+                }
+            }
+        } else {
+            Target = "World";
+        }
         System.out.println("Hello, " + Target + "!");
     }
 }
-
-
-
 
